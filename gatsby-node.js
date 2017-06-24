2d2778f15d20253e1c6f5ed27a58eb38980ac7b4
Charlotte Dann <ask@pouretrebelle.com>
2017-06-24 18:34:40 +0100

    :wrench: Configure stylus as a css preprocessor

---
+++ gatsby-node.js
@@ -0,0 +1,33 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+exports.modifyWebpackConfig = function (config, stage) {
+  config.removeLoader('css');
+
+  if (stage == 'develop') {
+    config.loader('css', function (cfg) {
+      cfg.test = /\.styl$/;
+      cfg.loader = 'style!css?minimize&sourceMap!postcss!stylus-loader';
+
+      return cfg;
+    });
+  }
+  else {
+    config.loader('styl', function (cfg) {
+      cfg.test = /\.styl$/;
+      cfg.loader = ExtractTextPlugin.extract([
+        'css?minimize&sourceMap',
+        'postcss',
+        'stylus-loader',
+      ]);
+
+      return cfg;
+    });
+
+    config.plugin('extract-styl',
+      ExtractTextPlugin,
+      ['styles.css']
+    );
+  }
+
+  return config;
+};
--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../css/main';
+import '../css/app.styl';
 
 import PageLink from '../components/common/PageLink';
 

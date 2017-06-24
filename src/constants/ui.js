debff19b402e4d119d7f8048cd12266cbe0f0ff5
Charlotte Dann <ask@pouretrebelle.com>
2017-06-25 00:03:04 +0100

    :lipstick: Make white elements less common

--- src/constants/ui.js
+++ src/constants/ui.js
@@ -1,5 +1,4 @@
 export const ACCENT_COLORS = [
-  '#FFFFFF',
   '#E5162E',
   '#555454',
 ];
--- src/utils/uiUtils.js
+++ src/utils/uiUtils.js
@@ -1,9 +1,11 @@
 import { ACCENT_COLORS } from 'src/constants/ui';
 import { randomMinMax } from 'src/utils/numberUtils';
 
-export const getRandomAccentColor = () => (
-  ACCENT_COLORS[Math.floor(Math.random()*ACCENT_COLORS.length)]
-);
+export const getRandomAccentColor = () => {
+  let color = ACCENT_COLORS[Math.floor(Math.random()*ACCENT_COLORS.length)]
+  if (Math.random() < 0.1) color = '#FFFFFF';
+  return color
+};
 
 export const getRandomXPosition = () => (
   `${randomMinMax(10, 90)}%`

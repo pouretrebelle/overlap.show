9f0e83f15b546f871a03555bf38b673f20b3e406
Charlotte Dann <ask@pouretrebelle.com>
2017-07-02 20:35:48 +0100

    :memo: Edit About page text

--- src/components/About/What.js
+++ src/components/About/What.js
@@ -9,7 +9,7 @@ const What = () => (
     </h2>
     <div className={styles.content}>
       <p>
-        With diversity in expertise, the Compart-2016 Goldsmiths University of London brings together exquisite works of coding, physical computing and art. This group of people are bringing a variety of expressions to the display, which is both complementing and conflicting each other as a miscellany in a house.
+        Overlap is the 2017 final degree show for the MA Computational Arts programme at Goldsmiths College, University of London. The weekend long exhibition explores the exciting new waves originating from the intersection of art and technology. It features ground-breaking installations, interactive virtual and augmented realities, and thought-provoking conceptual works by mixed disciplinary artists from fields as diverse as fine art, dance, photography, graphic design, puppetry, sound art, and architecture.
       </p>
     </div>
   </section>

3134c34ca9d999559cd1dc2e9a9d140bb5057f28
Charlotte Dann <ask@pouretrebelle.com>
2017-07-03 20:26:37 +0100

    :art: Sort artists by last name

--- src/utils/contentUtils.js
+++ src/utils/contentUtils.js
@@ -1,3 +1,13 @@
 export const getArtistsFromRoute = ({ pages }) => (
-  pages.filter(page => (page.data.type && page.data.type == 'artist'))
+  pages.filter(page =>
+    (page.data.type && page.data.type == 'artist')
+  ).sort((a, b) => {
+    // sort by last name
+    const a1 = a.data.name.split(' ')[1];
+    const b1 = b.data.name.split(' ')[1];
+    if (a1 < b1) return -1;
+    if (a1 > b1) return 1;
+
+    return 0;
+  })
 );

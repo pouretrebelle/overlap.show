b6838b8384f61960ebb06598e6a88ac4452d924d
Charlotte Dann <ask@pouretrebelle.com>
2017-07-03 20:12:33 +0100

    :memo: Changing social links

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,4 @@
 export const TWITTER_URL = 'https://twitter.com/overlapshow';
-export const INSTAGRAM_URL = 'https://www.instagram.com/overlapshow/';
-export const FACEBOOK_URL = 'https://www.facebook.com/goldcomparts/';
+export const INSTAGRAM_URL = 'https://www.instagram.com/overlap.show/';
+export const FACEBOOK_URL = 'https://www.facebook.com/overlap.show/';
 export const CLOUDINARY_URL = 'http://res.cloudinary.com/overlap-show/image/upload';

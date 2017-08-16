06af40b6f3a74d29c782ea4bf3c57919d93f256c
Charlotte Dann <ask@pouretrebelle.com>
2017-08-16 19:08:50 +0100

    :memo: Add Eventbrite link to Navigation

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -3,6 +3,8 @@ import Link from 'gatsby-link';
 
 import styles from './Navigation.module.styl';
 
+import { EVENTBRITE_URL } from '../../constants/urls';
+
 const Navigation = () => (
   <nav className={styles.navigation}>
     <Link to={'/about/'} className={styles.link}>
@@ -11,6 +13,9 @@ const Navigation = () => (
     <Link to={'/artists/'} className={styles.link}>
       Artists
     </Link>
+    <a href={EVENTBRITE_URL} className={styles.link}>
+      Tickets
+    </a>
   </nav>
 );
 
